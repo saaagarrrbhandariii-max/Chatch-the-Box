9a126789570fb97931fec7c4f959760796c40b26
saaagarrrbhandariii-max <saaagarrrbhandariii@gmail.com>
2026-07-02 07:40:50 +0545

    Refactor box click event listener and fix dimensions

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let score = 0;
-const box =
-    document.getElementById("box");
-const scoreText =
-    document.getElementById("score");
-box.addEventListener("click",function ()
-                     score++;
-scoreText.innerText = score;
-let x = Math.random()*
-    (window.innerwidth - 70);
-let y = Math.random()*
-    (window.innerheight - 70);
+const box = document.getElementById("box");
+const scoreText = document.getElementById("score");
 
-box.style.left = x + "px";
-box.style.top = y + 'px";})
+let score = 0;
+
+box.addEventListener("click", function () {
+  score++;
+  scoreText.innerText = score;
+
+  let x = Math.random() * (window.innerWidth - 70);
+  let y = Math.random() * (window.innerHeight - 70);
+
+  box.style.left = x + "px";
+  box.style.top = y + "px";
+});
     
