a5f7d2c2835137983f06cb4cee8768e2efb52b8a
saaagarrrbhandariii-max <saaagarrrbhandariii@gmail.com>
2026-07-02 08:09:05 +0545

    Fix syntax error in event listener closure

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const box = document.getElementById("box");
 const scoreText = document.getElementById("score");
-
 let score = 0;
-
 box.addEventListener("click", function () {
   score++;
   scoreText.innerText = score;
-
   let x = Math.random() * (window.innerWidth - 70);
   let y = Math.random() * (window.innerHeight - 70);
-
   box.style.left = x + "px";
   box.style.top = y + "px";
 });
